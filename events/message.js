afc8936ac4d5ddda9d08dfbf652447ee129ab044
Artholgar <arthurpairaud@gmail.com>
2021-03-11 15:24:55 +0100

    if the message contains 'opti' and 'pas opt' bot will react with :opti: and :pasopti:

--- events/message.js
+++ events/message.js
@@ -1,10 +1,13 @@
 module.exports = {
     run: (client, message) => {
-        if (message.content.includes("opti")) {  
+        var regexpOpti = /(?<!pas[ ]*)opti/;
+        var regexpPasOpti = /(?<=pas[ ]*)opti/;
+
+        if (regexpOpti.test(message.content)) {  
             const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'opti');
             message.react(reactionEmoji);
         }
-        if (message.content.includes("pas opti")) {
+        if (regexpPasOpti.test(message.content)) {
             const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'pasopti');
             message.react(reactionEmoji);
         }
