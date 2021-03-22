822000f0e550546bd846e3e1a53958e9d71a181d
Artholgar <arthurpairaud@gmail.com>
2021-03-22 22:08:39 +0100

    modif in index.js

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const CommandoClient = require('./client');
+const { prefix, token } = require('./config.json');
 
 const path = require('path') // on ajoute la librairie path
 const fs = require('fs')
-const dotenv = require('dotenv')
-
-const envConfig = dotenv.parse(fs.readFileSync('.env'))
-for (const k in envConfig) {
-    process.env[k] = envConfig[k]
-}
 
 const config = require('./config.json');
 
 const client = new CommandoClient({
     ws: { intents: ['GUILD_PRESENCES', 'GUILD_MEMBERS'] },
-	commandPrefix: 'fdp!', // Préfixe des commandes (ex: ?help)
+	commandPrefix: config.prefix, // Préfixe des commandes (ex: ?help)
 	owner: process.env.BOT_OWNER_ID, // ID de l'owner du bot, peut également être un tableau d'id pour plusieurs owners, ex: ['ID1', 'ID2']
     disableMentions: 'everyone', // Désactive, par sécurité, l'utilisation du everyone par le bot
     presence: {
